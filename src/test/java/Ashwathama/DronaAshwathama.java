a39238d93a264912b930a87c04e5b2157a0ab2bc
SwarnalataJena <jenaswarnalata600@gmail.com>
2024-01-06 12:31:03 +0530

    this is dronaashwathama class

---
+++ src/test/java/Ashwathama/DronaAshwathama.java
@@ -0,0 +1,11 @@
+package Ashwathama;
+
+public class DronaAshwathama {
+
+	public static void main(String[] args) {
+		System.out.println("Wish you happy new year");
+		System.out.println("Have prosperous new year");
+
+	}
+
+}
